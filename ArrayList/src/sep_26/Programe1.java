fc342b3ad1ae29a5dbc3569a468fa06b1b89c121
Dilip <jpdilipk@gmail.com>
2023-09-26 09:47:15 -0400

    initial commit

---
+++ ArrayList/src/sep_26/Programe1.java
@@ -0,0 +1,18 @@
+package sep_26;
+
+import java.util.ArrayList;
+
+public class Programe1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList <Integer> myarray = new ArrayList<Integer>(3);
+		System.out.println(myarray.size());
+		myarray.add(1);
+		myarray.add(5);
+		System.out.println(myarray);
+
+	}
+
+}
