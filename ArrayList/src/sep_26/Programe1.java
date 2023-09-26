01e51bc1e31235f1afe0ba5308002fc5b4055ec9
Dilip <jpdilipk@gmail.com>
2023-09-26 10:09:59 -0400

    add, remove, contains,isEmpty added

--- ArrayList/src/sep_26/Programe1.java
+++ ArrayList/src/sep_26/Programe1.java
@@ -7,12 +7,24 @@ public class Programe1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		ArrayList <Integer> myarray = new ArrayList<Integer>(3);
-		System.out.println(myarray.size());
-		myarray.add(1);
-		myarray.add(5);
-		System.out.println(myarray);
-
+		ArrayList <Integer> myarrayList1 = new ArrayList<Integer>(3);
+		System.out.println(myarrayList1.size());
+		myarrayList1.add(1);
+		myarrayList1.add(5);
+		System.out.println(myarrayList1);
+		myarrayList1.add(1,9);
+		System.out.println(myarrayList1);
+		
+		ArrayList<Integer> myarrayList2 = (ArrayList<Integer>) myarrayList1.clone();
+		System.out.println(myarrayList2);
+		
+		System.out.println(myarrayList2.isEmpty());
+		
+		System.out.println(myarrayList2.contains(9));
+		
+		System.out.println(myarrayList2.remove(2));
+		System.out.println(myarrayList2);
+		
 	}
 
 }
